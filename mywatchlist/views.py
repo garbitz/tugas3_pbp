b5b0d844944bb89683477eefa041bdd0406957b5
Muhammad Abizar Rachmanda <lookerlose@gmail.com>
2022-09-21 14:25:09 +0700

    Changes on data, added test units

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -25,6 +25,15 @@ def show_json_by_id(request, id):
     data = WatchlistItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_xml_by_id(requesr, id):
+def show_xml_by_id(request, id):
     data = WatchlistItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_item_by_id(request):
+    data_watchlist = WatchlistItem.objects.filter(pk=id)
+    context = {
+        'list': data_watchlist,
+        'nama': 'Muhammad Abizar Rachmanda',
+        'id' : '2106751133',
+    }
+    return render(request, "mywatchlist.html", context)
